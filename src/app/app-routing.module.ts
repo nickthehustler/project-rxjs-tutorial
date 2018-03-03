aef68ff96e8837745a1522cd6a85928830700d38
Nick Brown <nicholas.g.brown@gmail.com>
2018-03-03 13:29:21 -0500

    Added counter component for counter rxjs example

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,6 @@
+import {Routes } from '@angular/router';
+import { CounterButtonsComponent } from './counter-buttons/counter-buttons.component';
+
+export const appRoutes: Routes = [
+    {path: 'counter', component: CounterButtonsComponent}
+  ];
